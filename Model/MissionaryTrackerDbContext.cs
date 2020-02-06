a2bdc3b2faf00ea61244ea42164d728c5c6d5ea3
Cory <fizzy.gutsnipe@outlook.com>
2020-02-05 21:48:35 -0800

    Initial commit

---
+++ Model/MissionaryTrackerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MissionaryTrackerApi.Model
+{
+    public class MissionaryTrackerDbContext : DbContext
+    {
+        public MissionaryTrackerDbContext(DbContextOptions<MissionaryTrackerDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
